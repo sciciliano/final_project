f68315018be21a091c2a9a1bc6992d004c6e333c
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-09 00:33:38 +0200

    Update nuovo.py

--- nuovo.py
+++ nuovo.py
@@ -1,7 +1,28 @@
 import streamlit as st
+import json, requests
+from googletrans import Translator
+from random import choice
 
+st.title('Adjectives')
 
-count = 3
-if count not in st.session_state:
-  st.session_state[count] = -1
-st.write(st.session_state.count)
+if 'counter' not in st.session_state:
+    st.session_state.counter = 3
+if st.session_state.counter < 1:
+    st.title('Game Over!')
+    st.stop()
+
+translator = Translator()
+keywords = ['bella','lungo','agile']
+var = choice(keywords)
+
+adj_trans = translator.translate(var,dest='en')
+adj_answer = st.text_input('Write your answer here:','')
+
+if adj_answer is not None:
+    if adj_answer == adj_trans:
+        feedback = st.write('Right!')
+    elif adj_answer  != adj_trans:
+        feedback = st.write('Nope, try again!')
+        st.session_state.counter -= 1
+
+st.write(st.session_state.counter)
