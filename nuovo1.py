842c5a815fb033047680b5252f84f3c77294694f
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-11 17:58:09 +0200

    Update nuovo1.py

--- nuovo1.py
+++ nuovo1.py
@@ -4,16 +4,12 @@ st.title('Adjectives')
 
 if 'counter' not in st.session_state:
     st.session_state.counter1 = 3
-
-if st.session_state.counter1 < 1:
-    st.title('Game Over!')
-    st.stop()
-
-click1 = st.button('counter 1')
-
-
-if click1:
-    st.session_state.counter1 -=1
+    click1 = st.button('counter 1')
+    if click1:
+        st.session_state.counter1 -=1
+    if st.session_state.counter1 < 1:
+        st.title('Game Over!')
+        st.stop()
 
 
 st.write(st.session_state.counter1)
