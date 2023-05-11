3b4d029ac8b47841832134488272cd9982f1b9fb
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-12 00:10:30 +0200

    Update nuovo2.py

--- nuovo2.py
+++ nuovo2.py
@@ -3,17 +3,16 @@ import streamlit as st
 from googletrans import Translator
 
 word_input = st.text_input('inserisci la tua traduzione: ','')
-if 'input_word' not in st.session_state:
-  st.session_state['input_word'] = word_input
 
 translator = Translator()
 words = ['mela','pera','pomodoro']
-word_pic = choice(words)
-st.write(word_pic)
-word_trans = translator.translate(word_pic,src='it', dest= 'en')
+if 'word_pic' not in st.session_state:
+ st.session_state.word_pic = choice(words)
+st.write(st.session_state.word_pic)
+word_trans = translator.translate(st.session_state.word_pic,src='it', dest= 'en')
 
 if word_input:
-  if word_trans.text == st.session_state['input_word']:
+  if word_trans.text == word_input:
     st.write('Esatto!')
-  elif word_trans.text != st.session_state['input_word']:
+  elif word_trans.text != word_input:
     st.write('Sbagliato!')
