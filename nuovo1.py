c319eba477929b3dee479544de974d6eefc1df6b
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-11 19:49:29 +0200

    Update nuovo1.py

--- nuovo1.py
+++ nuovo1.py
@@ -3,8 +3,10 @@ import streamlit as st
 st.title('Adjectives')
 
 if 'counter' not in st.session_state:
-    st.session_state.counter1 = 3
-
+    st.session_state.counter = 3
+if st.session_state.counter < 1:
+    st.title('Game Over!')
+    st.stop()
 
 click1 = st.button('counter 1')
 if click1:
