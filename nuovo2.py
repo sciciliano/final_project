5f84f08ae5dc84ef73c0c50f2a4eba75e4238a61
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-11 23:36:29 +0200

    Update nuovo2.py

--- nuovo2.py
+++ nuovo2.py
@@ -11,7 +11,7 @@ word_trans = translator.translate(word_pic,src='it', dest= 'en')
 
 word_input = st.text_input('inserisci la tua traduzione: ','')
 
-if (word_trans and word_input):
+if word_input:
   if word_trans.text == word_input:
     st.write('Esatto!')
   elif word_trans.text != word_input:
