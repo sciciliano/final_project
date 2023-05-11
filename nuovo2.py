cc566ce219ee792aa1bd8def6f27217762b8d92b
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-11 23:29:37 +0200

    Create nuovo2.py

---
+++ nuovo2.py
@@ -0,0 +1,18 @@
+from random import choice
+import streamlit as st
+from googletrans import Translator
+
+words = ['mela','pera','pomodoro']
+word_pic = choice(words)
+st.write(word_pic)
+
+translator = Translator()
+word_trans = translator.translate(word_pic,src='it', dest= 'en')
+
+word_input = st.input_text('inserisci la tua traduzione: ','')
+
+if (word_trans and word_input):
+  if word_trans.text == word_input:
+    st.write('Esatto!')
+  elif word_trans.text != word_input:
+    st.write('Sbagliato!')
