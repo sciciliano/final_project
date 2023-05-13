9f528462b1d6cbda40ee1ad49a532f7c6b256dc6
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-13 23:40:51 +0200

    Update nuovo2.py

--- nuovo2.py
+++ nuovo2.py
@@ -25,7 +25,8 @@ def new_word(new_word):
   
 if word_input:
   if st.session_state.word_trans == word_input:
-    st.write('Esatto!')
-    new_word(choice(words))
+   new_word(choice(words))
+   st.write('Esatto!')
+    
   elif st.session_state.word_trans != word_input:
     st.write('Sbagliato!')
