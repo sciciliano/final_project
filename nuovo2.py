fd08372ae0c329263c2ef0735248c70c69350aae
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-13 23:23:33 +0200

    Update nuovo2.py

--- nuovo2.py
+++ nuovo2.py
@@ -14,14 +14,13 @@ trans = translator.translate(word_pic,src='it', dest= 'en')
 if 'word_trans' not in st.session_state:
  st.session_state.word_trans = trans.text
 
- def change_word(new_word):
+ def new_word(new_word):
   st.session_state.choice = new_word
 
   
 if word_input:
   if st.session_state.word_trans == word_input:
     st.write('Esatto!')
-    change_word(choice(words))
-    st.session_state.choice = choice(words)
+    new_word(choice(words))
   elif st.session_state.word_trans != word_input:
     st.write('Sbagliato!')
