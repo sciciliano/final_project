46e918e192a5a8a8ed4b1dbfe6dc384f9f3c537e
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-14 11:52:33 +0200

    Update nuovo2.py

--- nuovo2.py
+++ nuovo2.py
@@ -27,8 +27,8 @@ def new_word(new_word):
  new_trans = translator.translate(new_word,src='it', dest= 'en')
  st.session_state.word_trans = new_trans.text
 
-word_pic = st.session_state.choice
-pic_word = st.write(word_pic)
+ 
+st.write(st.session_state.choice)
 
   
 if word_input:
@@ -36,17 +36,15 @@ if word_input:
   st.session_state.counter1 -=1
   st.write('Sbagliato!')
  elif st.session_state.word_trans == word_input:
-  new_word(choice(words))
-  del pic_word
-  word_pic = st.session_state.choice
-  pic_word = st.write(word_pic)
+  #new_word(choice(words))
+  #st.write(st.session_state.choice)
   st.write('Esatto! Ora passiamo agli antonimi!')
   keyword=st.text_input('scrivi qui l\' antonimo: ','')
-  url= 'https://api.datamuse.com/words?rel_ant=' + keyword + ''
+  url= 'https://api.datamuse.com/words?rel_ant=' + st.session_state.word_trans + ''
   response = requests.get(url)
   datamuse = json.loads(response.text)
   if 'antonym' not in st.session_state:
-   st.session_state.antonym = datamuse[1]['word']
+   st.session_state.antonym = datamuse[0]['word']
   st.write(st.session_state.antonym)
 
 st.write(st.session_state.counter1)
