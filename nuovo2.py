e06bc222d8b13a9a3f02173498156bcf7086a0c4
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-14 10:01:12 +0200

    Update nuovo2.py

--- nuovo2.py
+++ nuovo2.py
@@ -8,20 +8,16 @@ word_input = st.text_input('inserisci la tua traduzione: ','')
 words = ['mela','pera','pomodoro']
 if 'choice' not in st.session_state:
  st.session_state.choice = choice(words)
+ 
+def new_word(new_word):
+ st.session_state.choice = new_word
+ 
 word_pic = st.session_state.choice
-pic_word = st.empty()
-pic_word.text(word_pic)
+pic_word = st.write(word_pic)
 
 translator = Translator()
 trans = translator.translate(word_pic,src='it', dest= 'en')
 if 'word_trans' not in st.session_state:
- st.session_state.word_trans = trans.text
- 
-def new_word(new_word):
- st.session_state.choice = new_word
- word_pic = st.session_state.choice
- pic_word.text(word_pic)
- trans = translator.translate(word_pic,src='it', dest= 'en')
  st.session_state.word_trans = trans.text
 
 
